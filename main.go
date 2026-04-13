ccbe34be8c27cee311768f1d00b8db7c8f54dff5
Eivan <clevextog88@gmail.com>
2026-04-13 22:37:15 +0300

    Storage: Create+GetUser

--- main.go
+++ main.go
@@ -1,1 +1,23 @@
-sdfsdfsdfsdf
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	connStr := "host=localhost port=5432 user=postgres password=qwerty12345 dbname=restaraunt_db sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal("error opening database: ", err)
+	}
+	defer db.Close()
+	err = db.Ping()
+	if err != nil {
+		log.Fatal("error connecting to databse: ", err)
+	}
+	fmt.Println("connected to database successfully")
+}
---
+++ models.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"time"
+)
+
+type User struct {
+	ID           int
+	Email        string
+	PasswordHash string
+	CreatedAt    time.Time
+}
