8b0ac6ccb8fa753ffbfee39248bc519ca6da8c35
Eivan <clevextog88@gmail.com>
2026-04-15 21:31:18 +0300

    Logic: Login

--- logic.go
+++ logic.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"errors"
+
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -28,6 +30,28 @@ func Register(email, password string) error {
 	err = CreateUser(email, passwordHash)
 	if err != nil {
 		return err
+	}
+	return nil
+}
+
+func Login(email, password string) error {
+	err := validEmail(email)
+	if err != nil {
+		return err
+	}
+	if password == "" {
+		return errors.New("password is required")
+	}
+	user, err := GetUserByEmail(email)
+	if err == ErrUserNotFound {
+		return ErrInvalidCredentials
+	}
+	if err != nil {
+		return err
+	}
+	err = bcrypt.CompareHashAndPassword([]byte(user.PasswordHash), []byte(password))
+	if err != nil {
+		return ErrInvalidCredentials
 	}
 	return nil
 }
