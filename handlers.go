c441d49be15bf0ef863214d6fcfafc03d45d587e
Eivan <clevextog88@gmail.com>
2026-04-19 16:25:06 +0300

    add RegisterHandler

---
+++ handlers.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func writeJSON(w http.ResponseWriter, status int, data interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	json.NewEncoder(w).Encode(data)
+}
+
+func fallback(w http.ResponseWriter, r *http.Request) {
+	writeJSON(w, http.StatusNotFound, ErrorResponse{Error: "not found"})
+}
+
+func RegisterHandler(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+	var req RegistrationRequest
+	dec := json.NewDecoder(r.Body)
+	dec.DisallowUnknownFields()
+	err := dec.Decode(&req)
+	if err != nil {
+		writeJSON(w, http.StatusBadRequest, ErrorResponse{Error: "bad request"})
+		return
+	}
+	err = Register(req.Email, req.Password)
+	if err == ErrUserExists {
+		writeJSON(w, http.StatusConflict, ErrorResponse{Error: "user already exists"})
+		return
+	}
+	if err == ErrEmailValidation {
+		writeJSON(w, http.StatusBadRequest, ErrorResponse{Error: "email validation failed"})
+		return
+	}
+	if err == ErrPasswordValidation {
+		writeJSON(w, http.StatusBadRequest, ErrorResponse{Error: "password validation failed"})
+		return
+	}
+	if err != nil {
+		writeJSON(w, http.StatusInternalServerError, ErrorResponse{Error: "internal server error"})
+		return
+	}
+	writeJSON(w, http.StatusCreated, MessageResponse{Message: "registration was successful"})
+}
